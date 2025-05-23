2d2f5ad6b5ceb3fb86760aa925f58af68b087e53
kisutoo <malk-om@outlook.fr>
2025-05-23 16:57:31 +0200

    e

--- ex 3/js/script.js
+++ ex 3/js/script.js
@@ -2,28 +2,29 @@ let tourne1 = document.querySelector('.carre1');
 let tourne2 = document.querySelector('.carre2');
 let tourne3 = document.querySelector('.carre3');
 let tourne4 = document.querySelector('.carre4');
-let index = 0
 
-
-tourne1.addEventListener('click', () => transformation(tourne1));
-tourne2.addEventListener('click', () => transformation(tourne2));
-tourne3.addEventListener('click', () => transformation(tourne3));
-tourne4.addEventListener('click', () => transformation(tourne4));
-
-// tourne1.addEventListener('click', () => detransformation(tourne1));
-// tourne2.addEventListener('click', () => detransformation(tourne2));
-// tourne3.addEventListener('click', () => detransformation(tourne3));
-// tourne4.addEventListener('click', () => detransformation(tourne4));
+tourne1.addEventListener('click', () => transformation(tourne1), false);
+tourne2.addEventListener('click', () => transformation(tourne2), false);
+tourne3.addEventListener('click', () => transformation(tourne3), false);
+tourne4.addEventListener('click', () => transformation(tourne4), false);
 
 function transformation(test)
 {
-    test.style.transform = "rotate(-40deg)";
-    test.style.backgroundColor = "rgb(182, 82, 82)";
-    test.style.height = "180px";
-    test.style.width = "180px"
+    let index = 0
+    if(index == 0)
+    {
+        index++;
+        test.style.transform = "rotate(-40deg)";
+        test.style.backgroundColor = "rgb(182, 82, 82)";
+        test.style.height = "180px";
+        test.style.width = "180px";
+    }
+    if(index > 1)
+    {
+        index--;
+        test.style.transform = "rotate(40deg)";
+        test.style.backgroundColor = "rgb(46, 122, 97)";
+        test.style.height = "200px";
+        test.style.width = "200px"
+    }
 }
-
-
-
-
-
